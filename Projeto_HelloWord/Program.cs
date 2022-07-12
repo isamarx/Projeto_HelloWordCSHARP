d527c5b781c9f29b29f3ae80b02a74af76af91c6
Samara Pereira <samarapds26@gmail.com>
2022-07-12 19:46:32 -0300

    Hello World

---
+++ Projeto_HelloWord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
